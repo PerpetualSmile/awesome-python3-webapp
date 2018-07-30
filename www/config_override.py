b9c6d8515bea043a521d0dba901a3cf2cb48e08d
PerpetualSmile <shenjundong1996@gmail.com>
2018-07-30 18:00:40 +0800

    7.30

---
+++ www/config_override.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python3 
+# -*- coding: utf-8 -*- 
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '',
+        'db': 'awesome'
+    }
+}
--- www/test.py
+++ www/test.py
@@ -4,12 +4,11 @@ import asyncio
 
 
 async def test(loop):
-    await orm.create_pool(loop, user='root', password='', db='awesome')
-    u = User(name='Test', email='test@example.com', passwd='12345678', image='about:blank')
+    await orm.create_pool(loop, user='', password='', db='')
+    u = User(name='Test', email='testeee@example.com', passwd='12345678', image='about:blank')
     await u.save()
 
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(test(loop))
 loop.close()
-
